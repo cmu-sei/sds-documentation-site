39aece50c21af3b36fe6bf47e07cc40e8895fa36
Jason D Shimkoski <jdshimkoski@sei.cmu.edu>
2025-12-11 09:56:20 -0500

    fix: footer logo in dark mode fix: icon spacing for page title fix: update example appSuite name

--- .playground/app/app.config.ts
+++ .playground/app/app.config.ts
@@ -4,21 +4,25 @@ export default defineAppConfig({
   },
   githubUrl: 'https://github.com',
   appSuitePrefix: 'SEI',
-  appSuite: 'SEI',
+  appSuite: 'SDS',
   pageTitle: 'Documentation Site',
   // navigation: [
   //   { title: 'Home', path: '/' },
-  //   { title: 'Getting Started', path: '/getting-started', children: [
-  //       { title: 'Installation', path: '/guides/installation' },
-  //       { title: 'Configuration', path: '/guides/configuration' }
-  //     ] },
+  //   {
+  //     title: 'Getting Started',
+  //     path: '/getting-started',
+  //     children: [
+  //       { title: 'asdfasdf', path: '/guides/installation' },
+  //       { title: 'fewfwef', path: '/guides/configuration' }
+  //     ]
+  //   },
   //   {
   //     title: 'Guides', path: '/guides', children: [
   //       { title: 'Installation', path: '/guides/installation' },
   //       { title: 'Configuration', path: '/guides/configuration' }
   //     ]
   //   },
-  //   { title: 'API Reference', path: '/api' }
+  //   { title: 'API Reference', path: '/api' },
   // ],
   footerLinks: [
     { title: 'Contact Us', path: 'https://sei.cmu.edu/contact-us/' },
